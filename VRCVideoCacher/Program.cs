2d41b6ce8cf16e070901c97ef98822420a6582b4
EllyVR <git@ellyvr.dev>
2024-12-09 05:42:14 -0600

    Version Bump

--- VRCVideoCacher/Program.cs
+++ VRCVideoCacher/Program.cs
@@ -12,7 +12,7 @@ namespace VRCVideoCacher;
 internal static class Program
 {
     public static string ytdlpHash = string.Empty;
-    public const string Version = "2024.11.28";
+    public const string Version = "2024.12.9";
     public static readonly ILogger Logger = Log.ForContext("SourceContext", "Core");
     public static async Task Main(string[] args)
     {
