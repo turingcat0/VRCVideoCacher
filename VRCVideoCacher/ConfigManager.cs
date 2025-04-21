c62e09398f0691849f5b68d11983cab06786982a
EllyVR <git@ellyvr.dev>
2025-04-21 18:48:15 -0500

    Create First Run Menu

--- VRCVideoCacher/ConfigManager.cs
+++ VRCVideoCacher/ConfigManager.cs
@@ -16,6 +16,7 @@ public class ConfigManager
         if (!File.Exists(ConfigFileName))
         {
             Config = new ConfigModel();
+            FirstRun();
             SaveConfig(true);
         }
         else
@@ -32,6 +33,35 @@ public class ConfigManager
         if (!silent)
             Log.Information("Saving config...");
         File.WriteAllText(ConfigFileName, JsonConvert.SerializeObject(Config, Formatting.Indented));
+    }
+    
+    public static void FirstRun()
+    {
+        Log.Information("It appears this is your first time running VRCVideoCacher. Lets create a basic config file. ");
+        Log.Information("Would you like to cache Youtube Videos? (y/n)");
+        var input = Console.ReadLine();
+        if (input?.ToLower() == "y")
+            Config.CacheYouTube = true;
+        else
+            Config.CacheYouTube = false;
+        Log.Information("Would you like to cache PyPyDance Videos? (y/n)");
+        input = Console.ReadLine();
+        if (input?.ToLower() == "y")
+            Config.CachePyPyDance = true;
+        else
+            Config.CachePyPyDance = false;
+        Log.Information("Would you like to cache VRDancing Videos? (y/n)");
+        input = Console.ReadLine();
+        if (input?.ToLower() == "y")
+            Config.CacheVRDancing = true;
+        else
+            Config.CacheVRDancing = false;
+        Log.Information("Would you like to autogenerate a PoToken? (Requires Chrome to be installed) (y/n)");
+        input = Console.ReadLine();
+        if (input?.ToLower() == "y")
+            Config.ytdlGeneratePoToken = true;
+        else
+            Config.ytdlGeneratePoToken = false;
     }
 }
 
