413c882e0fe117e1c1bdf8d8c906f1d1e36c8d8c
EllyVR <git@ellyvr.dev>
2025-04-22 06:50:44 -0500

    Fix Unassigned Local Variable when compiled Release

--- VRCVideoCacher/Updater.cs
+++ VRCVideoCacher/Updater.cs
@@ -22,7 +22,7 @@ public class Updater
     public static async Task CheckForUpdates()
     {
         Log.Information("Checking for updates...");
-        bool isDebug;
+        bool isDebug = false;
 #if DEBUG
             isDebug = true;
 #endif
