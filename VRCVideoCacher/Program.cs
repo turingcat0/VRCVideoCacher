ee877c6566c213b8677d7679004bca890d562e1c
EllyVR <git@ellyvr.dev>
2025-05-18 12:07:43 -0500

    Version Bump!

--- VRCVideoCacher/Program.cs
+++ VRCVideoCacher/Program.cs
@@ -11,7 +11,7 @@ namespace VRCVideoCacher;
 internal static class Program
 {
     public static string YtdlpHash = string.Empty;
-    public const string Version = "2025.5.14";
+    public const string Version = "2025.5.18";
     public static readonly string CurrentProcessPath = Path.GetDirectoryName(Environment.ProcessPath) ?? string.Empty;
     public static readonly ILogger Logger = Log.ForContext("SourceContext", "Core");
     
